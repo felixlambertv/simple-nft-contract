03552e5ef9ae028263f3619b483df900c34715f5
Felix Lambert <felix@itsavirus.com>
2022-09-07 14:48:34 +0800

    add mumbai network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const { ALCHEMY_API_KEY_MUMBAI } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    mumbai: {
+      url: ALCHEMY_API_KEY_MUMBAI,
+      accounts: [
+        "8bde43799f301447ab03e6e25f3db094599b15acf836077c1d98a9b66aa8c37d",
+      ],
+    },
+  },
 };
 
 export default config;
